16bc3ed9222dcf7b47975364bfaa1e202e43ec62
Nigel <nigel.oketch@student.moringaschool.com>
2023-06-12 23:39:32 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-function scuberGreetingForFeet(){
+var returnResponse = "";
+
+function scuberGreetingForFeet(distanceInFeet){
   // Write your code here!
+	if (distanceInFeet > 2000) {
+		returnResponse = 'No can do.';
+	}
+	else if (distanceInFeet > 1500) {
+		returnResponse = 'I will gladly take your thirty bucks.';
+	}
+	else if (distanceInFeet <= 300) {
+		returnResponse = 'This one is on me!';
+	}
+	return returnResponse;
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(cityName){
   // Write your code here!
+	cityName === 'NYC' ? returnResponse = 'Ok, sounds good.' : returnResponse = 'No go.';
+	return returnResponse;
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tipAmount){
   // Write your code here!
+	switch (tipAmount) {
+		case 'generous' : returnResponse = 'Thank you so much.'; break;
+		case 'not as generous': returnResponse = 'Thank you.'; break;
+		default : returnResponse = 'Bye.';
+	}
+	return returnResponse;
 }
